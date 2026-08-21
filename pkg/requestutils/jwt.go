21ed1d2027368ac742ea0c9f9afdb1ffcf769bae
VVVietAnh <157110579+HemlockPham7@users.noreply.github.com>
2026-08-21 20:15:33 +0700

    add warning for jwt

--- pkg/requestutils/jwt.go
+++ pkg/requestutils/jwt.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/newrelic/go-agent/v3/newrelic"
 )
 
 var (
@@ -13,6 +14,11 @@ var (
 	ErrEmptyID      = errors.New("empty id in token claims")
 	ErrNotExist     = errors.New("claim not exist")
 )
+
+var PrivateKeyCompromiseWarning = newrelic.Error{
+	Message: "JWT has invalid format - Private key might be compromised!",
+	Class:   "JWTError",
+}
 
 func GetUserIDFromRequest(c *gin.Context) (string, error) {
 	claims, exist := c.Get("claims")
@@ -31,6 +37,7 @@ func GetUserIDFromRequest(c *gin.Context) (string, error) {
 
 	uid, ok := tokenInfo["sub"].(string)
 	if !ok {
+		newrelic.FromContext(c).NoticeError(PrivateKeyCompromiseWarning)
 		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "invalid token"})
 		c.Abort()
 		return "", ErrInvalidToken
